35619522e32bb3b90a9bcb9397ad7e79ca30ea9d
DONGJU <aeyongworld@gmail.com>
2024-01-08 21:13:43 +0900

    chapter02 완료
    react로 웹페이지 기본 틀 구성

--- src/router/root.js
+++ src/router/root.js
@@ -1,10 +1,15 @@
 import { Suspense, lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
+import todoRouter from "./todoRouter";
 
 const Loading = <div>Loading...</div>
 const Main = lazy(() => import("../pages/MainPage"))
 
 const About = lazy(() => import("../pages/AboutPage"))
+
+const TodoIndex = lazy(() => import("../pages/todo/IndexPage"))
+
+const TodoList = lazy(() => import("../pages/todo/ListPage"))
 
 const root = createBrowserRouter([
 
@@ -15,6 +20,11 @@ const root = createBrowserRouter([
     {
         path: "about",
         element: <Suspense fallback={Loading}><About/></Suspense>
+    },
+    {
+        path: "todo",
+        element: <Suspense fallback={Loading}><TodoIndex/></Suspense>,
+        children: todoRouter()
     }
 ])
 
